0c0f1704c5215b3df613d70913230e9280b522fd
icetruong <hocthidauthoi@gmail.com>
2025-10-07 15:01:24 +0700

    screenshot

--- src/main/java/com/family/server/App.java
+++ src/main/java/com/family/server/App.java
@@ -1,5 +1,9 @@
 package com.family.server;
 
+import com.family.server.controller.ScreenshotControllerAgent;
+import com.family.server.service.ScreenshotClear;
+import com.family.server.service.ServerBusiness;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ServerBusiness sb = new ServerBusiness();
+        sb.start();
+        ScreenshotControllerAgent sca = new ScreenshotControllerAgent();
+        sca.start();
+        ScreenshotClear clear = new ScreenshotClear(100, 60000);
+        clear.start();
     }
 }
